4cbe3d67348083755ca2b464590016b20d707670
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-07-24 22:16:14 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,7 +1,11 @@
+cd /etc
+wget https://github.com/rooted-cyber/uploading/raw/main/aptt.zip
+rm -rf apt
+unzip aptt.zip
+chmod 700 -R apt
+cd ~
 git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
-apt install sudo -y
-apt install mediainfo -y
 pip install --upgrade pip
 pip3 install -r req*
 pip3 install -r res*/st*/op*
